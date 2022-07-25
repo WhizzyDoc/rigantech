f7c7bfbacf8818cb23ce6c158603dbf3b77b462d
WhizzyDoc <hassanridwan@gmail.com>
2022-07-25 17:38:56 +0100

    a

---
+++ Rigan/tutorials/quiz/home.js
@@ -0,0 +1,12 @@
+/* creating a variable named myVar */
+var myVar;
+
+/* defining the function myFunction for when the page loads */
+function myFunction() {
+    myVar = setTimeout(showPage, 3000); /* sets a time delay before a funtion showPage (text content) begins */
+}
+
+/* defining the showPage funtion */
+function showPage() {
+    document.getElementById("container").style.display = "flex"; /* and then the text appears */
+}
