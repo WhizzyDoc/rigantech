79794827e6eb31321b2c1b3c27ee0a158140470e
WhizzyDoc <hassanridwan@gmail.com>
2022-08-02 19:10:33 +0100

    a

--- Rigan/tutorials/stylesheet/code.js
+++ Rigan/tutorials/stylesheet/code.js
@@ -2,9 +2,20 @@ var html = document.getElementById("html");
 var css = document.getElementById("css");
 var output = document.getElementById("output");
 var header = document.getElementById("header");
+var color = document.getElementById("color");
+var rgb = document.getElementById("rgb");
 
 function runCode() {
     output.innerHTML = html.value;
     document.getElementById("style").innerHTML = css.value;
 }
 
+function hexToRgb() {
+    let Color = color.value;
+    let x = [];
+    color = Color.replace('#', '')
+    x.push(parseInt(color.slice(0, 2), 16))
+    x.push(parseInt(color.slice(2, 4), 16))
+    x.push(parseInt(color.slice(4, 6), 16))
+    rgb.innerHTML = "rgb(" + x.toString() + ")";
+}
